a5465b6149a74efc7d14f31d6d8ee51465a7563e
Lexy Sokolowski <a.j.sokolowski@wustl.edu>
2025-04-01 13:02:30 -0500

    Added a new top level component Game to display a list of previous moves

--- src/App.jsx
+++ src/App.jsx
@@ -55,6 +55,18 @@ export default function Board() {
     </>
   );
 }
+export default function Game() {
+  return (
+    <div className="game">
+      <div className="game-board">
+        <Board />
+      </div>
+      <div className="game-info">
+        <ol>{/*TODO*/}</ol>
+      </div>
+    </div>
+  );
+}
 
 function calculateWinner(squares) {
   const lines = [
